4983dbcf70cc2a86a5f56bde7d38544cca56888f
Arghya Banerjee <arghya.banerjee.dev@gmail.com>
2022-01-17 16:13:03 +0530

    Initial Commit

---
+++ project/main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font, colorchooser, filedialog, messagebox
+import os
+
+main_application = tk.Tk()
+main_application.geometry("960x540")
+main_application.title("Dark Pad")
+
+
+
+
+main_application.mainloop()
